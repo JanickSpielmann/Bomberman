49511a89e1d8a68ee5d7f217f00ca0f704e339da
Miguel Angelo Mendes Pereira <mme138648@iet-gibb.ch>
2023-12-04 12:44:19 +0100

    teil1 &  hälfte teil2

--- src/main/java/application/server/control/BombermanServer.java
+++ src/main/java/application/server/control/BombermanServer.java
@@ -5,6 +5,4 @@ public class BombermanServer {
     public static void main(String[] args) {
 //new
     }
-
-
 }
---
+++ src/main/java/application/server/network/MessageEntry.java
@@ -0,0 +1,11 @@
+package application.server.network;
+
+import network.Message;
+import network.server.ServerApplicationInterface;
+
+public class MessageEntry implements ServerApplicationInterface {
+    @Override
+    public void handleMessage(Message message, String connectionId) {
+
+    }
+}
